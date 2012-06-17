162849d1d839ff1e5db5b256fad489bd814b8d96
Darcy Laycock <sutto@sutto.net>
2012-06-17 15:35:13 +0800

    Specs!

--- spec/pyapns2_spec.rb
+++ spec/pyapns2_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe Pyapns2 do
 
-  let(:config) { TestConfiguration.instance }
+  use_vcr_cassette
 
+  let(:config)  { TestConfiguration.instance }
+  let(:app)     { config.fake_app }
   let(:options) { config.provisioning_options }
   let(:client)  { Pyapns2.new config.host, config.port }
 
@@ -15,15 +17,30 @@ describe Pyapns2 do
       expect { client.provision options.merge(app_id: 123) }.to raise_error ArgumentError
     end
 
-    it 'should raise an argument error with an invalid cert'
+    it 'should raise an argument error with an invalid cert' do
+      expect { client.provision options.merge(cert: nil) }.to raise_error ArgumentError
+      expect { client.provision options.merge(cert: "") }.to raise_error ArgumentError
+      expect { client.provision options.merge(cert: 123) }.to raise_error ArgumentError
+    end
 
-    it 'should raise an argument error with an invalid timeout'
+    it 'should raise an argument error with an invalid timeout' do
+      expect { client.provision options.merge(timeout: nil) }.to raise_error ArgumentError
+      expect { client.provision options.merge(timeout: "10") }.to raise_error ArgumentError
+      expect { client.provision options.merge(timeout: -10) }.to raise_error ArgumentError
+    end
 
-    it 'should raise an argument error with an invalid environment'
+    it 'should raise an argument error with an invalid environment' do
+      expect { client.provision options.merge(env: nil) }.to raise_error ArgumentError
+      expect { client.provision options.merge(env: "awesome") }.to raise_error ArgumentError
+      expect { client.provision options.merge(env: "staging") }.to raise_error ArgumentError
+      expect { client.provision options.merge(env: 1) }.to raise_error ArgumentError
+    end
 
     context 'hitting the server' do
 
-      it 'should return true'
+      it 'should return true' do
+        client.provision(options).should be_true
+      end
 
     end
 
@@ -31,27 +48,57 @@ describe Pyapns2 do
 
   describe 'notifying a user' do
 
-    it 'should raise an error with an invalid app_id'
+    let(:token)        { config.token }
+    let(:notification) { {aps: {alert: "Hello from tests!"}} }
 
-    it 'should raise an error with invalid tokens'
+    before :each do
+      # Provision a client that we're able to use to send notifications.
+      client.provision options
+    end
 
-    it 'should raise an error with invalid notifications'
+    it 'should raise an error with an invalid app_id' do
+      expect do
+        client.notify 'this-is-a-fake-app', token, notification
+      end.to raise_error Pyapns2::Error
+    end
 
-    it 'should raise an error with mismatched types'
+    it 'should raise an error with invalid tokens' do
+      expect { client.notify app, nil, notification }.to raise_error ArgumentError
+      expect { client.notify app, 123, notification }.to raise_error ArgumentError
+    end
+
+    it 'should raise an error with invalid notifications' do
+      expect { client.notify app, token, nil }.to raise_error ArgumentError
+      expect { client.notify app, token, "test-notification" }.to raise_error ArgumentError
+    end
+
+    it 'should raise an error with mismatched types' do
+      expect { client.notify app, [token], notification }.to raise_error ArgumentError
+      expect { client.notify app, token, [notification] }.to raise_error ArgumentError
+    end
 
     context 'hitting the server' do
 
-      it 'should raise an exception if notifications fail'
+      it 'should raise an exception if notifications fail' do
+        expect { client.notify 'non-existing-app', token, notification }.to raise_error Pyapns2::Error
+        expect { client.notify app, ("a" * 5), notification }.to raise_error Pyapns2::Error
+      end
 
-      it 'should return true if the notification works'
+      it 'should return true if the notification works' do
+        client.notify(app, token, notification).should be_true
+      end
 
-      it 'should work with a hash'
+      it 'should work with a hash' do
+        client.notify app, token => notification
+      end
 
-      it 'should work with a single notification'
+      it 'should work with a single notification' do
+        client.notify app, token, notification
+      end
 
-      it 'should work with an array'
-
-      it 'should return the error with an unknown app_id'
+      it 'should work with an array' do
+        client.notify app, [token], [notification]
+      end
 
     end
 
@@ -59,13 +106,21 @@ describe Pyapns2 do
 
   describe 'getting feedback' do
 
-    it 'should raise an error with an invalid app_id'
+    it 'should raise an error with an invalid app_id' do
+      expect { client.feedback nil }.to raise_error ArgumentError
+    end
 
     context 'htting the server' do
 
-      it 'should correctly return an array'
+      it 'should correctly return an array' do
+        client.feedback(app).should be_a Array
+      end
 
-      it 'should return the error with an invalid app id'
+      it 'should return the error with an invalid app id' do
+        expect { client.feedback 1 }.to raise_error Pyapns2::Error
+        expect { client.feedback "unknown-app-id" }.to raise_error Pyapns2::Error
+        expect { client.feedback "id" }.to raise_error Pyapns2::Error
+      end
 
     end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,15 +11,15 @@ class TestConfiguration
 
   include Singleton
 
-  attr_reader :host, :port, :token, :cert_path, :fake_app
+  attr_accessor :host, :port, :token, :cert_path, :fake_app
 
   def initialize
-    fake_token = (1..64).map { |t| rand(16).to_s(16) }.join
-    self.host = (ENV['PYAPNS_HOST'] || 'localhost')
-    self.port = (ENV['PYAPNS_PORT'] || 7077).to_i
-    self.token = (ENV['TEST_PUSH_TOKEN'] || fake_token)
+    fake_token     = (1..64).map { |t| rand(16).to_s(16) }.join
+    self.host      = (ENV['PYAPNS_HOST'] || 'localhost')
+    self.port      = (ENV['PYAPNS_PORT'] || 7077).to_i
+    self.token     = (ENV['TEST_PUSH_TOKEN'] || fake_token)
     self.cert_path = File.expand_path "../certificates/#{(ENV['PYAPNS_CERT'] || "fake")}.pem", __FILE__
-    self.fake_app = "fake-app-#{Time.now.to_i}"
+    self.fake_app  = "fake-app-#{Time.now.to_i}"
   end
 
   def cert
@@ -28,7 +28,7 @@ class TestConfiguration
 
   def provisioning_options
     {
-      app_id:      app_id,
+      app_id:      fake_app,
       cert:        cert,
       environment: 'sandbox',
       timeout:     15
@@ -37,19 +37,18 @@ class TestConfiguration
 
 end
 
-p $pyapns_default_options
-
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
-  c.default_cassette_options = {:record => :new_episodes}
+  c.default_cassette_options = {match_requests_on: [:method, :uri, :body], :record => :new_episodes}
   # Filter out parts of the test
   config = TestConfiguration.instance
-  %w(pyapns_host pyapns_port notification_token pyapns_fake_app).each do |var_name|
+  %w(host port token cert fake_app).each do |var_name|
     c.filter_sensitive_data("{#{var_name}}") { config.send var_name }
   end
 end
 
 RSpec.configure do |config|
   config.mock_with :rr
+  config.extend VCR::RSpec::Macros
 end
